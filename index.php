e602a6a0671e02a17f9fb047727fdd0f39d64d48
2521802005 <2521802005@qq.com>
2019-07-11 08:58:23 +0800

    day5

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
- function fbnq($n){
- 	if($n<=0);return 0;
- 	if($n==1||$n==2);return 1;
- 	return fbnq($n-1)+fbnq($n-2);
- }
+	$arr = [1,2,3,4,5,6];
+	function number($arr){
+		//偶数
+		$ou = [];
+		$ji = [];
+		for($i=0;$i<count($arr);$i++){
+			if($arr[$i]%2==1){
+				$ji[] = $arr[$i];
+			}else{
+				$ou[] = $arr[$i];
+			}
+		}
+		return array_merge($ji,$ou);
+	}
+	print_r(number($arr));
